8e2e344ad46af983150b427fe7954364f9c51141
Garagekeeper <hj06869@naver.com>
2022-06-10 15:15:54 +0900

    stock 추가
    stock 추가

--- repository/stock.repository.js
+++ repository/stock.repository.js
@@ -1,9 +1,84 @@
 const model = require("../models");
 
-const Stock = {};
+const Stock = function(stock){
+    this.branch_id = stock.branch_id
+    this.stuff_name = stock.stuff_name;
+    this.stock_num = stock.stock_num;
+    this.stock_id = stock.stock_id;
+    this.expired_date = stock.expired_date;
+};
 
-Stock.findOneStuff = (branch, results) => {
+Stock.getAllList = async(newStock, results) => {
+    if(!newStock.stuff_name){
+        await model.Stock.findAll({
+            raw:true,
+            where:{branch_id:newStock.branch_id}
+        }).then(result=>{
+            console.log("Stock retrieving done");
+            results(null,result);
+        }).catch(err=>{
+            console.log("err occured while getting stock list");
+            results(err,null);
+        })
 
+    }else{
+        await model.Stuff.findOne({
+            raw:true,
+            where:{stuff_name:newStock.stuff_name},
+            attributes:['stuff_id'],
+        }).then(stuff=>{
+            model.findAll({
+                raw:true,
+                where:{branch_id:newStock.branch_id, stuff_id:stuff.stuff_id},
+            }).then(result=>{
+                console.log("Stock retrieving done");
+                results(null,result);
+            }).catch(err=>{
+                console.log("err occured while getting stock list with"+newStock.branch_id+", "+newStock.stuff_name);
+                results(err,null);
+            })
+        }).catch(err=>{
+            console.log("err occured while getting stuff_id (Stock.getAllList with:"+ newStock.stuff_name+")");
+            results(err,null);
+        })
+    }
 }
 
-Stock.findAllStuff
+Stock.addItem = async(newStock, results)=>{
+    await model.Stuff.findOne({
+        raw:true,
+        where:{stuff_name:newStock.stuff_name},
+        attributes:['stuff_id'],
+    }).then(stuff=>{
+        model.Stock.create({
+            stuff_id:stuff.stuff_id,
+            stock_num:newStock.stock_num,
+            branch_id:newStock.branch_id,
+            expired_date:newStock.expired_date
+        }).then(result=>{
+            console.log("Stock create done");
+            results(null,newStock);
+        }).catch(err=>{
+            console.log("err occured while creating stock");
+        results(err,null);
+        })
+    }).catch(err=>{
+        console.log("err occured while getting stuff_id (Stock.addItem with:"+ stock.stuff_name+")");
+        results(err,null);
+    })
+}
+
+Stock.updateStock = async(newStock, results) =>{
+    await model.Stock.update(
+        {stock_num:newStock.stock_num},
+        {where: {branch_id:newStock.branch_id, stock_id:newStock.stock_id}
+    }).then(result=>{
+        console.log("Upadating stock done with:"+newStock.stock_id)
+        results(null,{msg:"Update complete"});
+    }).catch(err=>{
+        console.log("err occured while updating stock");
+        results(err,null);
+    })
+}
+
+module.exports = Stock;
